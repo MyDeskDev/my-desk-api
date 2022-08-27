b62d297f2511b82ee51a33dac79d44f61d6dec9e
dev-yhc <hyunchul.yang@gmail.com>
2022-08-27 18:53:11 +0900

    add profile controller

--- src/main/java/com/mydesk/api/common/controller/HealthCheckController.java
+++ src/main/java/com/mydesk/api/common/controller/HealthCheckController.java
@@ -1,13 +1,33 @@
 package com.mydesk.api.common.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class HealthCheckController {
 
+    private final Environment env;
+
     @GetMapping("/ping")
-    public String healthCheck() {
+    public String health() {
         return "pong";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> activeProfiles  = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = activeProfiles.isEmpty() ? "default" : activeProfiles.get(0);
+
+        return activeProfiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
